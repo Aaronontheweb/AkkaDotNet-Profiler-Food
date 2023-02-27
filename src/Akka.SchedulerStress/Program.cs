afaf8dbe168005ca2538578c63104811e3278b22
Aaron Stannard <aaron@petabridge.com>
2023-02-26 18:35:42 -0600

    complete v1

--- src/Akka.SchedulerStress/Program.cs
+++ src/Akka.SchedulerStress/Program.cs
@@ -1,5 +1,11 @@
-﻿var actorSystem = ActorSystem.Create("MyActorSystem");
+﻿using Akka.SchedulerStress;
 
+var actorSystem = ActorSystem.Create("MyActorSystem");
+var logger = Logging.GetLogger(actorSystem, typeof(ClockDriftRunnable));
 var scheduler = actorSystem.Scheduler;
 
-scheduler.Advanced.ScheduleRepeatedly(TimeSpan.FromMilliseconds(0), TimeSpan.FromMilliseconds(20));
+var runnable = new ClockDriftRunnable(DateTime.UtcNow, logger, TimeSpan.FromMilliseconds(20));
+
+scheduler.Advanced.ScheduleRepeatedly(TimeSpan.FromMilliseconds(0), TimeSpan.FromMilliseconds(20), runnable);
+
+await actorSystem.WhenTerminated;
